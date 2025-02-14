a6d0b3fd9f1af6090b7670fbe20c953c7d4dc25d
tsztin0217 <tsztin0217@gmail.com>
2025-02-13 21:25:27 -0500

    Completed up to Exercise 7

--- app.js
+++ app.js
@@ -107,6 +107,38 @@ name: Mewtwo, type: psychic, hp: 106
 // adding MewTwo, Rhydon, and Snorlax!
 game.party.push('MewTwo', 'Rhydon', 'Snorlax');
 
-console.log(game)
+/*
+Exercise 6
+1. Set the `completed` property to true for gyms with a difficulty below 3.
+2. Think about how you'd loop through the `gyms` array to check and update the `completed` property.
 
-  
+
+Solve Exercise 6 here:
+*/
+
+// loop through gyms array
+for (const gym of game.gyms) {
+    if (gym.difficulty < 3) {
+        gym.completed = true;
+    }
+}
+
+
+/*
+Exercise 7
+1. Evolve the starter Pokémon you added to your party earlier. Each starter Pokémon evolves into a specific one.
+2. How would you replace the current starter Pokémon in your party with its evolved form?
+
+Hint: 
+  - Pokemon 1: Bulbasaur evolves into Pokemon 2: Ivysaur
+  - Pokemon 4: Charmander evolves into Pokemon 5: Charmeleon
+  - Pokemon 7: Squirtle evolves into Pokemon 8: Wartortle
+  - Pokemon 25: Pikachu evolves into Pokemon 26: Raichu
+
+More Hints: The existing starter Pokemon will be *replaced* in your party with the Pokemon it evolved into. When working with an array of objects, the splice() array method is ideal for replacing one element with another. 
+
+
+Solve Exercise 7 here:
+*/
+
+game.party.splice(0, 1, 'Raichu');
